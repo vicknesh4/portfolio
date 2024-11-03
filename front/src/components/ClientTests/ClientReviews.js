c66bd7f59717091b657599d8410a544930b3bfec
Gajanraj <gajanraj.mohanaraj@epitech.eu>
2024-11-03 22:53:35 +0100

    push project front

--- front/src/components/ClientTests/ClientReviews.js
+++ front/src/components/ClientTests/ClientReviews.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './ClientReviews.css';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
+const API_URL = process.env.REACT_APP_API_URL ;
 
 const ClientReviews = () => {
   const [reviews, setReviews] = useState([]);
